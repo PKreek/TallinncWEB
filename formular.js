77d93064846efbc645c5b0cc0bf9b5f6c91b8917
NikAntKar <96243019+NikAntKar@users.noreply.github.com>
2022-09-21 10:30:28 +0200

    fix

--- formular.js
+++ formular.js
@@ -1,8 +1,8 @@
-function validering()
+function valideringEmail()
 {
-   
-    var form = document.getElementById("form")
     var email = document.getElementById("email").value
+    var emailChecken=document.getElementById("emailCheck")
+    var textEmail = document.getElementById("errorEmail")
     
 
 
@@ -14,17 +14,59 @@ function validering()
 
     if(email.match(pattern))
     {
-        form.classList.add("valid")
-        form.classList.remove("invalid")
+       
+        emailChecken.classList.add("valid")
+        emailChecken.classList.remove("invalid")
+        textEmail.innerHTML = "Du har anget en korrekt epost!";
+        textEmail.style.color= "#00ff00";
     }
     else
     {
-        form.classList.remove("valid")
-        form.classList.add("invalid")
+    emailChecken.classList.remove("valid")
+    emailChecken.classList.add("invalid")
+    textEmail.innerHTML = "Ange korrekt email";
+    textEmail.style.color= "#ff0000";
     }
-    
-    console.log(form)
-    console.log(email);
+if(email =="")
+{
+    emailChecken.classList.remove("valid")
+    emailChecken.classList.add("invalid")
+    textEmail.innerHTML = "";
+    textEmail.style.color= "#00ff00";
+}
+}
+
+
+function valideringNamn()
+{
+var name=document.getElementById("name").value
+var nameCheck=document.getElementById("nameCheck")
+var textNamn=document.getElementById("errorNamn")
+
+console.log(name);
+if(name.length > 30 )
+{
+    console.log(30);
+    nameCheck.classList.remove("valid")
+    nameCheck.classList.add("invalid")
+    textNamn.innerHTML="Max antal ord är 30. Vänligen ange inte fler"
+    textNamn.style.color="#ff0000";
+}
+
+else
+{
+    nameCheck.classList.add("valid")
+    nameCheck.classList.remove("invalid")
+    textNamn.innerHTML="Du har angett ett tillåtet namn!"
+    textNamn.style.color="#00ff00";
+}
+if(name =="")
+{
+    nameCheck.classList.remove("valid")
+    nameCheck.classList.add("invalid")
+    textNamn.innerHTML = "";
+    textNamn.style.color= "#00ff00";
+}
 }
 
 
