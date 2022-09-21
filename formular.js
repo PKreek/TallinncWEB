6d71152eca1ef07ada33a041a57b3ade2aa64e46
noahjarvback <96242977+noahjarvback@users.noreply.github.com>
2022-09-21 09:33:36 +0200

    Revert "formulär forts"
    This reverts commit 756008947ca4d25cb28033026682ef61c6ba7b05.

--- formular.js
+++ formular.js
@@ -1,66 +1,30 @@
-function valideringEmail()
+function validering()
 {
+   
+    var form = document.getElementById("form")
     var email = document.getElementById("email").value
-    var emailChecken=document.getElementById("emailCheck")
-    var textEmail = document.getElementById("errorEmail")
     
-    var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
 
-    console.log(email)
+
+    var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    
+    console.log(form)
+    console.log(email);
+
 
     if(email.match(pattern))
     {
-        emailChecken.classList.add("valid")
-        emailChecken.classList.remove("invalid")
-        textEmail.innerHTML = "Du har anget en korrekt epost!";
-        textEmail.style.color= "#00ff00";
+        form.classList.add("valid")
+        form.classList.remove("invalid")
     }
     else
     {
-        emailChecken.classList.remove("valid")
-        emailChecken.classList.add("invalid")
-        textEmail.innerHTML = "Ange korrekt email";
-        textEmail.style.color= "#ff0000";
+        form.classList.remove("valid")
+        form.classList.add("invalid")
     }
-    if(email =="")
-    {
-        emailChecken.classList.remove("valid")
-        emailChecken.classList.add("invalid")
-        textEmail.innerHTML = "";
-        textEmail.style.color= "#00ff00";
-    }
-}
-
-function valideringNamn()
-{
-    var name=document.getElementById("name").value
-    var nameCheck=document.getElementById("nameCheck")
-    var textNamn=document.getElementById("errorNamn")
-
-    console.log(name);
-
-    if(name.length>30 )
-    {
-      
-        nameCheck.classList.remove("valid")
-        nameCheck.classList.add("invalid")
-        textNamn.innerHTML="Max antal ord är 30. Vänligen ange inte fler"
-        textNamn.style.color="#ff0000";
-    }
-    if(name =="")
-    {
-        nameCheck.classList.remove("valid")
-        nameCheck.classList.add("invalid")
-        textNamn.innerHTML = "";
-        textNamn.style.color= "#00ff00";
-    }
-    else
-    {
-        nameCheck.classList.add("valid")
-        nameCheck.classList.remove("invalid")
-        textNamn.innerHTML="Du har angett ett tillåtet namn!"
-        textNamn.style.color="#00ff00";
-    }
+    
+    console.log(form)
+    console.log(email);
 }
 
 
