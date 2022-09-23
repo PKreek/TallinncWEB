7abf69921aaed5e0febd019a47b7740c51126c6b
markwest7 <markus.westberg@gmail.com>
2022-09-23 10:01:20 +0200

    skillbars

---
+++ barscript.js
@@ -0,0 +1,46 @@
+skillDisplay = document.getElementById('skillbar')
+skillArray = []
+
+const addSkill = (description, skillLevel) => {
+    skillArray.push({
+        desc: description,
+        skill: skillLevel
+    })
+}
+
+const displaySkills = () => {
+    skillArray.forEach(aSkill => {
+        const para = document.createElement('p')
+        const skillText = document.createTextNode(aSkill.desc)
+        para.appendChild(skillText)
+        skillDisplay.appendChild(para)    
+
+        const barContainer = document.createElement('div')
+        barContainer.classList.add('skillBarContainer')
+        const bar = document.createElement('div')
+        bar.classList.add('skillBar')
+        animateSkills(bar, aSkill.skill, 0)
+        barContainer.appendChild(bar)
+        skillDisplay.appendChild(barContainer)
+    });  
+}
+
+const animateSkills = (bar, skill, counter) => {
+   
+    bar.style.width = `${counter}%`
+    console.log(counter)
+    counter++
+
+    if(counter<skill){
+        setTimeout(()=>{animateSkills(bar, skill, counter)}, 15)
+    }
+
+}
+
+
+addSkill ('Javascript', 78)
+addSkill ('HTML', 53)
+addSkill ('CSS', 18)
+displaySkills()
+
+console.log(skillArray)
